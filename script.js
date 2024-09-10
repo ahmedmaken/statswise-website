cf76e49e715c9797023d7c2ba5edb99deb1bc3d1
ahmedmukhtar <ahm.maken@gmail.com>
2024-09-10 23:05:05 +0100

    Initial commit of StatsWise website

---
+++ script.js
@@ -0,0 +1,69 @@
+// GSAP Scroll-trigger animations
+gsap.registerPlugin(ScrollTrigger);
+
+// Animating Home Section Heading and Button
+gsap.from(".home-section h1", {
+    duration: 1.5,
+    y: 100,
+    opacity: 0,
+    ease: "power3.out",
+});
+
+gsap.from(".home-section p", {
+    duration: 1.5,
+    delay: 0.5,
+    y: 100,
+    opacity: 0,
+    ease: "power3.out",
+});
+
+gsap.from(".home-section a", {
+    duration: 1.5,
+    delay: 1,
+    y: 100,
+    opacity: 0,
+    ease: "power3.out",
+});
+
+// Animating Service Section
+gsap.from("#services h2", {
+    scrollTrigger: {
+        trigger: "#services",
+        start: "top center",
+    },
+    duration: 1,
+    y: 50,
+    opacity: 0,
+});
+
+gsap.from(".services-section .col-md-4", {
+    scrollTrigger: {
+        trigger: ".services-section",
+        start: "top 75%",
+    },
+    duration: 1.5,
+    opacity: 0,
+    stagger: 0.3,
+    ease: "power2.out",
+});
+
+// About and Contact Sections Animation
+gsap.from("#about h2", {
+    scrollTrigger: {
+        trigger: "#about",
+        start: "top center",
+    },
+    duration: 1,
+    y: 50,
+    opacity: 0,
+});
+
+gsap.from("#contact h2", {
+    scrollTrigger: {
+        trigger: "#contact",
+        start: "top center",
+    },
+    duration: 1,
+    y: 50,
+    opacity: 0,
+});
