2174435d0c34d7f9535893663c41af695e474f7b
ahmedmukhtar <ahm.maken@gmail.com>
2024-09-12 01:00:58 +0100

    changes

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 // Register GSAP ScrollTrigger Plugin
 gsap.registerPlugin(ScrollTrigger);
 
+document.addEventListener('DOMContentLoaded', function () {
+    // Show the banner if the user has not interacted with it
+    if (!localStorage.getItem('cookieConsentStatus')) {
+        document.getElementById('cookieConsentBanner').style.display = 'block';
+    }
+
+    // Handle accept
+    document.getElementById('acceptCookies').addEventListener('click', function () {
+        localStorage.setItem('cookieConsentStatus', 'accepted');
+        document.getElementById('cookieConsentBanner').style.display = 'none';
+    });
+
+    // Handle decline
+    document.getElementById('declineCookies').addEventListener('click', function () {
+        localStorage.setItem('cookieConsentStatus', 'declined');
+        document.getElementById('cookieConsentBanner').style.display = 'none';
+    });
+});
+
 // Animating Home Section Heading and Button
 gsap.from(".home-section h1", {
     duration: 1.5,
@@ -98,3 +117,4 @@ if (window.innerWidth <= 768) { // Adjust based on your mobile breakpoint
         opacity: 0,
     });
 }
+
